d75173446949fc1bbc06556e64473a243caa961f
Bugnariu Dan <dan.bugnariu0902@gmail.com>
2023-04-11 23:32:45 +0300

    synchronizing threads for the same process

--- a2/a2.c
+++ a2/a2.c
@@ -1,8 +1,14 @@
-#include <stdio.h>
-#include <stdio.h>
-#include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <semaphore.h>
 #include "a2_helper.h"
 
 void proc_1()
@@ -17,8 +23,81 @@ void proc_3()
 void proc_4()
 {
 }
+
+typedef struct
+{
+    int id;
+    int *proc_5_thread_2_start;
+    int *proc_5_thread_1_end;
+    sem_t *sem;
+} proc_5_thread_data;
+void *proc_5_thread(void *arg)
+{
+    int id = ((proc_5_thread_data *)arg)->id;
+    int *proc_5_thread_2_start = ((proc_5_thread_data *)arg)->proc_5_thread_2_start;
+    int *proc_5_thread_1_end = ((proc_5_thread_data *)arg)->proc_5_thread_1_end;
+    sem_t *sem = ((proc_5_thread_data *)arg)->sem;
+    free(arg);
+
+    if (id == 1)
+    {
+        int ok=0;
+        while (!ok)
+        {
+            sem_wait(sem);
+            ok = *proc_5_thread_2_start;
+            sem_post(sem);
+        }
+    }
+
+    info(BEGIN, 5, id);
+    if (id == 2)
+    {
+        sem_wait(sem);
+        *proc_5_thread_2_start = 1;
+        sem_post(sem);
+        int ok = 0;
+        while (!ok)
+        {
+            sem_wait(sem);
+            ok = *proc_5_thread_1_end;
+            sem_post(sem);
+        }
+    }
+    info(END, 5, id);
+
+    if (id == 1)
+    {
+        sem_wait(sem);
+        *proc_5_thread_1_end = 1;
+        sem_post(sem);
+    }
+    return NULL;
+}
 void proc_5()
 {
+    pthread_t threads[5];
+    sem_t sem;
+    sem_init(&sem, 0, 1);
+    int proc_5_thread_2_start = 0;
+    int proc_5_thread_1_end = 0;
+
+    for (int i = 0; i < 5; i++)
+    {
+        proc_5_thread_data *data = malloc(sizeof(proc_5_thread_data));
+        data->id = i + 1;
+        data->proc_5_thread_2_start = &proc_5_thread_2_start;
+        data->proc_5_thread_1_end = &proc_5_thread_1_end;
+        data->sem = &sem;
+
+        pthread_create(&threads[i], NULL, proc_5_thread, data);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        pthread_join(threads[i], NULL);
+    }
+
+    sem_destroy(&sem);
 }
 void proc_6()
 {
